a6512814522cb02387aa6325b9ecff13a39dacde
komemo <msn_07zgok@yahoo.co.jp>
2018-10-09 23:19:28 +0900

    Initial commit

---
+++ src/module.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// class クラスネームで class を作成する
+class Human {
+  // constructor はクラスからインスタンスを作成した時に実行される
+  constructor(name, age) {
+    // this は作成されたインスタンスを指す
+    this.name = name;
+    this.age = age;
+  }
+
+  // クラスメソッド
+  // クラスが持つファンクションのこと
+  callMyProfile() {
+    // 自分自身の値を参照するためにここでも this を使う
+    console.log(this.name, this.age);
+  }
+}
+
+const Nakanishi = new Human("Nakanishi", 17);
+
+callMyProfile();
