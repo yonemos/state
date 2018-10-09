bda4c109feff4283eb1e9980fad50a26ae424c92
komemo <msn_07zgok@yahoo.co.jp>
2018-10-09 23:34:08 +0900

    講座１９
    クリックされた時にsetStateでstateを変化する

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
-import { module } from "./module";
+
+class Human extends React.Component{
+  constructor(props){
+   super(props);
+   this.state={name:"unko"};
+  }
+  render(){
+    return(
+      <h2 onClick={this.onButtonClick}>
+      {this.state.name}{this.props.age}</h2>
+    );
+  }
+  onButtonClick(){
+    alert("click")
+  }
+}
+
+render(<Human age="30" />,document.getElementById("root"))
